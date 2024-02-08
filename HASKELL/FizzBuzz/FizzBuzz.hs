e6ef9e58af87136d3b20bfe5181dc79ff555173c
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-08 16:14:58 -0600

    Se finalizo la funcion number

--- HASKELL/FizzBuzz/FizzBuzz.hs
+++ HASKELL/FizzBuzz/FizzBuzz.hs
@@ -10,5 +10,31 @@ fizzbuzz :: Int -> String
 -- fizzbuzz n = "One!"
 --fizzbuzz n = if n==1 then "One!" else "Two!"
 -- fizzbuzz n = ifThenElse (n==1) "One!" "Two!"
-fizzbuzz n |  (n==1) = "One!"
-           | (n/=1) = "Two!"
+fizzbuzz n = lessThan20(n)
+fizzbuzz _ = "othewise"
+
+lessThan20 :: Int -> String
+lessThan20 n
+            | n > 0 && n < 20 =
+            let answers = words ("one two three four five six seven eight nine ten " ++
+                    "eleven twelve thirteen fourteen fifteen sixteen " ++
+                    "seventeen eighteen nineteen")
+            in answers !! (n-1)
+
+tens :: Int -> String
+tens n  
+    | n>=2 && n <= 9 =
+        answers!!(n-2)
+        where
+            answers = words  "twenty thirty forty fifty sixty seventy eighty ninety"
+
+number :: Int -> String
+number n 
+    | n>=1 && n<20 = lessThan20(n)
+    | n `mod` 10 == 0 && n < 100 = tens(n `div` 10) 
+    | n < 100  = tens(n `div` 10) ++ " " ++ lessThan20(n `mod` 10)
+    | n == 100 = "One Hudred!"
+ 
+-- fizzbuzz n |  (n==1) = "One!"
+--            | (n/=1) = "Two!"
+
