c1631c76fab00af64c7917574823cfb93d658aee
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-13 16:59:03 -0600

    Avance en Kata Bowling

--- HASKELL/Bowling/Bowling.hs
+++ HASKELL/Bowling/Bowling.hs
@@ -6,4 +6,11 @@ data Frame = Open Int Int
     deriving (Eq, Show)
 
 toFrames :: [Int] -> [Frame]
-toFrames pins = replicate 10 (Open 0 0)
+toFrames pins = go 1 pins
+    where
+        go 10 [x, y] = [Open x y]
+        go 10 [x, y, z]
+            | x + y == 10 = [Spare x z]
+        go n (x:y:z:ys)
+            | x + y == 10 = Spare x z : go (n+1) (z:ys)
+            | otherwise = Open x y : go (n+1) (z:ys)
