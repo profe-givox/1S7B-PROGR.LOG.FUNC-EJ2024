c9f51217a88bca654a1b21656dfa36ee670eacc3
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-19 16:19:40 -0600

    Avance: Bowling con Maybe

--- HASKELL/Bowling/Bowling.hs
+++ HASKELL/Bowling/Bowling.hs
@@ -5,12 +5,19 @@ data Frame = Open Int Int
     | Strike Int Int
     deriving (Eq, Show)
 
-toFrames :: [Int] -> [Frame]
+toFrames :: [Int] -> Maybe [Frame]
 toFrames pins = go 1 pins
     where
-        go 10 [x, y] = [Open x y]
+        go 10 [x, y]
+            | x + y < 10 = Just [Open x y]
+            | otherwise = Nothing
         go 10 [x, y, z]
-            | x + y == 10 = [Spare x z]
+            | x == 10 = Just [Strike y z]
+            | x + y == 10 = Just [Spare x z]
+            | otherwise = Nothing
         go n (x:y:z:ys)
-            | x + y == 10 = Spare x z : go (n+1) (z:ys)
-            | otherwise = Open x y : go (n+1) (z:ys)
+            | x == 10 = fmap (Strike y z :) $ go (n+1) (y:z:ys)
+            | x + y == 10 = fmap (Spare x z :) $ go (n+1) (z:ys)
+            | x + y < 10 = fmap (Open x y :) $ go (n+1) (z:ys)
+            | otherwise = Nothing
+        go _ _ = Nothing
